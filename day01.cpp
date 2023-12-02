a801ac362471568fa60ca78c7f3f9c36cd0da8a0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-02 12:03:43 +0000

    day 1 - part 2

--- day01.cpp
+++ day01.cpp
@@ -11,20 +11,46 @@ constexpr bool digit(char c) {
     return false;
   return true;
 }
+constexpr bool starts_with(jute::view str, jute::view seed) {
+  auto [l, r] = str.subview(seed.size());
+  return l == seed;
+}
+static_assert(starts_with("one", "one"));
+static_assert(!starts_with("one", "ones"));
+static_assert(starts_with("onetwo", "one"));
+static_assert(!starts_with("onetwo", "two"));
+
+constexpr int digit(jute::view v) {
+  constexpr const jute::view names[10] = {"zero",  "one",  "two", "three",
+                                          "four",  "five", "six", "seven",
+                                          "eight", "nine"};
+  if (digit(v[0])) {
+    return v[0] - '0';
+  }
+  for (auto i = 0; i < 10; i++) {
+    if (starts_with(v, names[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main() {
   int sum{};
   loop([&](auto line) {
     int val{};
-    for (auto c : line) {
-      if (!digit(c))
-        continue;
-      if (val == 0) {
-        val = (c - '0') * 10;
-        val += (c - '0');
-      } else {
-        val -= val % 10;
-        val += (c - '0');
+    while (line.size() > 0) {
+      auto d = digit(line);
+      if (d >= 0) {
+        if (val == 0) {
+          val = d * 10;
+          val += d;
+        } else {
+          val -= val % 10;
+          val += d;
+        }
       }
+      line = line.subview(1).after;
     }
     info("val", val);
     sum += val;
