209c12d89829b243ab2f417e4ee1ebfe511dd9f2
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-05 17:56:54 +0000

    day 05 - part 2 (example works)

--- day05.cpp
+++ day05.cpp
@@ -5,17 +5,40 @@ import jute;
 import silog;
 
 struct seed {
-  long value{};
+  long start{};
+  long end{};
   int map{};
 };
+constexpr bool operator==(const seed &a, const seed &b) {
+  return a.start == b.start && a.end == b.end && a.map == b.map;
+}
+
 hai::varray<seed> seeds{1024};
 bool got_seeds{};
 int cur_map{};
 
+struct spair {
+  seed a;
+  seed b;
+};
+constexpr bool operator==(const spair &a, const spair &b) {
+  return a.a == b.a && a.b == b.b;
+}
+constexpr auto split(seed s, long p) {
+  spair res{s, s};
+  res.a.end = p;
+  res.b.start = p + 1;
+  res.b.end = s.end;
+  return res;
+}
+
 void run(jute::view line) {
   if (!got_seeds) {
-    for (auto s : atoi_it(line.split(':').after)) {
-      seeds.push_back(seed{s});
+    auto it = atoi_it(line.split(':').after).begin();
+    for (; it != atoi_it{}; ++it) {
+      auto s = *it;
+      auto l = *++it;
+      seeds.push_back(seed{s, s + l - 1});
     }
     got_seeds = true;
     return;
@@ -23,6 +46,7 @@ void run(jute::view line) {
   if (line == "")
     return;
   if (line.index_of(':') > 0) {
+    info("map", line);
     cur_map++;
     return;
   }
@@ -34,21 +58,51 @@ void run(jute::view line) {
   for (auto &s : seeds) {
     if (s.map == cur_map)
       continue;
-    if (from_s <= s.value && s.value <= from_e) {
-      s.value = to_s + s.value - from_s;
-      s.map = cur_map;
+
+    silog::log(silog::debug, "%d %d -- %ld %ld === %d", from_s, from_e, s.start,
+               s.end, to_s);
+    if (s.end < from_s)
+      continue;
+    if (from_e < s.start)
+      continue;
+
+    s.map = cur_map;
+    if (from_s <= s.start && s.end <= from_e) {
+      silog::log(silog::debug, "------ %ld %ld", s.start, s.end);
+    } else if (from_s <= s.start) {
+      auto [a, b] = split(s, from_e);
+      silog::log(silog::debug, "------ << %ld %ld -- %ld %ld", a.start, a.end,
+                 b.start, b.end);
+      s = a;
+      seeds.push_back(b);
+    } else if (s.end <= from_e) {
+      auto [a, b] = split(s, from_s - 1);
+      silog::log(silog::debug, "------ >> %ld %ld -- %ld %ld", a.start, a.end,
+                 b.start, b.end);
+      s = b;
+      seeds.push_back(a);
+    } else {
+      auto [a, b] = split(s, from_s - 1);
+      auto [c, d] = split(b, from_e);
+      silog::log(silog::debug, "------ %ld %ld -- %ld %ld -- %ld %ld", a.start,
+                 a.end, c.start, c.end, d.start, d.end);
+      s = c;
+      seeds.push_back(a);
+      seeds.push_back(d);
     }
+    s.start = to_s + s.start - from_s;
+    s.end = to_s + s.end - from_s;
   }
 }
 
 int main() {
-  loop(run);
+  loop_e(run);
 
   long min{};
   for (auto s : seeds) {
     if (min == 0)
-      min = s.value;
-    mn(min, s.value);
+      min = s.start;
+    mn(min, s.start);
   }
   info("res", min);
 }
