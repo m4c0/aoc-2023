3910d0d7d1b2a19500762b0c5356c5bb526196d9
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-10 11:26:03 +0000

    day 10 - part 2

--- day10.cpp
+++ day10.cpp
@@ -7,18 +7,23 @@ import silog;
 hai::varray<jute::view> lines{1024};
 char at(point p) { return lines[p.y][p.x]; }
 
+hai::array<char> purged{};
+int pstride;
+char &pat(point p) { return purged[p.y * pstride + p.x]; }
+
 enum cardinal { X = -1, N, S, W, E };
+constexpr const cardinal cloop[4]{N, S, W, E};
 constexpr auto step(cardinal c) {
   constexpr const point vals[4]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
   return vals[c];
 }
+constexpr const jute::view map[4]{
+    "| 7F",
+    " |JL",
+    "LF- ",
+    "J7 -",
+};
 constexpr void next(point &p, cardinal &card) {
-  constexpr const jute::view map[4]{
-      "| 7F",
-      " |JL",
-      "LF- ",
-      "J7 -",
-  };
   card = static_cast<cardinal>(map[card].index_of(at(p)));
   if (card == X) {
     silog::log(silog::error, "ops");
@@ -33,8 +38,16 @@ bool run(point s, cardinal c) {
   if (valids[c].index_of(at(p)) < 0)
     return false;
 
+  for (auto cc : cloop) {
+    if (valids[cc].index_of(at(s + step(cc))) < 0)
+      continue;
+
+    pat(s) = map[c ^ 1][cc];
+  }
+
   int n{1};
   while (s != p) {
+    pat(p) = at(p);
     next(p, c);
     n++;
   }
@@ -57,6 +70,43 @@ int main(int argc, char **argv) {
 
     lines.push_back(l);
   }
+  pstride = lines[0].size();
+  purged.set_capacity(lines.size() * pstride);
 
-  run(s, W) || run(s, E) || run(s, N) || run(s, S);
+  for (auto c : cloop)
+    if (run(s, c))
+      break;
+
+  for (auto &c : purged)
+    if (c == 0)
+      c = ' ';
+
+  int res{};
+  char *p = purged.begin();
+  for (auto i = 0; i < lines.size(); i++) {
+    bool in{};
+    char flip{};
+    for (auto j = 0; j < pstride; j++, p++) {
+      if (*p == '|') {
+        in = !in;
+      } else if (*p == flip) {
+        in = !in;
+        flip = 0;
+      } else if (*p == ' ' && in) {
+        *p = '#';
+        res++;
+      } else {
+        constexpr const jute::view flips = "FJL7";
+        auto idx = flips.index_of(*p);
+        if (idx >= 0 && !flip) {
+          flip = flips[idx ^ 1];
+        } else if (idx >= 0) {
+          flip = 0;
+        }
+      }
+    }
+
+    silog::log(silog::debug, "[%.*s]", pstride, p - pstride);
+  }
+  info("res", res);
 }
