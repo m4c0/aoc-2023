c0ff96ef67fce930f1cd96912a978b00e7734988
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-13 13:10:01 +0000

    day 13 - part 1 - working for test data

--- day13.cpp
+++ day13.cpp
@@ -7,37 +7,56 @@ import silog;
 struct map {
   int orig[20];
   int trans[20];
+  int r;
+  int c;
 };
 
-int check(const int (&m)[20]) {
-  for (auto i : m) {
-    silog::log(silog::info, "%08x", i);
+int check(const int (&m)[20], int n) {
+  for (auto i = 0; i < n - 1; i++) {
+    silog::log(silog::debug, "%x %x %d/%d", m[i], m[i + 1], i, n);
+    if (m[i] != m[i + 1])
+      continue;
+
+    auto j = 0;
+    for (; j < i && j + i + 1 < n; j++) {
+      silog::log(silog::debug, "%x %x %d %d", m[i - j], m[i + j + 1], j,
+                 i + j + 1);
+      if (m[i - j] != m[i + j + 1])
+        break;
+    }
+    if (i + j + 1 == n) {
+      info("i", i);
+      return i + 1;
+    }
   }
-  silog::log(silog::info, "-=-=-=-=-=-=");
+  info("meh", 0);
   return 0;
 }
-int run(const map &m) { return check(m.orig) + check(m.trans); }
+int run(const map &m) {
+  auto r = check(m.orig, m.r) * 100 + check(m.trans, m.c);
+  if (r == 0)
+    throw 0;
+  return r;
+}
 
 int main(int argc, char **argv) {
   auto dt = data::of(argc);
 
   int res{};
   map m{};
-  int r{};
   for (auto row : dt) {
     if (row == "") {
       res += run(m);
       m = {};
-      r = 0;
       continue;
     }
-    int c = 0;
+    m.c = 0;
     for (auto chr : row) {
-      m.orig[r] = (m.orig[r] << 1) | (chr == '#' ? 1 : 0);
-      m.trans[c] = (m.trans[c] << 1) | (chr == '#' ? 1 : 0);
-      c++;
+      m.orig[m.r] = (m.orig[m.r] << 1) | (chr == '#' ? 1 : 0);
+      m.trans[m.c] = (m.trans[m.c] << 1) | (chr == '#' ? 1 : 0);
+      m.c++;
     }
-    r++;
+    m.r++;
   }
   res += run(m);
   info("res", res);
