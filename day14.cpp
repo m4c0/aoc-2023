a0018ff4261ac88ab968872dba03c0ba1ab9ddc0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-15 10:29:49 +0000

    day 14 - part 2 - pre-process the map before count

--- day14.cpp
+++ day14.cpp
@@ -13,6 +13,37 @@ int main(int argc, char **argv) {
     cols = row.size();
     rows++;
   }
+  hai::array<char> map{rows * cols};
+  auto p = map.begin();
+  for (auto row : dt) {
+    for (auto c : row)
+      *p++ = c;
+  }
+
+  for (auto i = 0; i < rows; i++)
+    silog::log(silog::debug, "%.*s", cols, map.begin() + i * cols);
+
+  for (auto c = 0; c < cols; c++) {
+    int rr = 0;
+    for (auto r = 0; r < rows; r++) {
+      switch (map[c + r * cols]) {
+      case '.':
+        break;
+      case 'O':
+        map[c + r * cols] = '.';
+        map[c + rr * cols] = 'O';
+        rr++;
+        break;
+      case '#':
+        rr = r + 1;
+        break;
+      }
+    }
+  }
+
+  silog::log(silog::debug, "");
+  for (auto i = 0; i < rows; i++)
+    silog::log(silog::debug, "%.*s", cols, map.begin() + i * cols);
 
   hai::array<long> pts{cols};
   for (auto &p : pts) {
@@ -21,7 +52,8 @@ int main(int argc, char **argv) {
 
   int r{};
   long res{};
-  for (auto row : dt) {
+  for (auto i = 0; i < rows; i++) {
+    auto row = map.begin() + r * cols;
     for (int c{}; c < cols; c++) {
       if (row[c] == 'O') {
         res += pts[c]--;
