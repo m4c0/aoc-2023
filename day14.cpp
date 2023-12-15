50d295c8f198617c8a70dde2594bfc29a19b4ff0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-15 11:29:14 +0000

    day 14 - part 2 - done

--- day14.cpp
+++ day14.cpp
@@ -4,6 +4,82 @@ import hai;
 import jute;
 import silog;
 
+class map_t {
+  unsigned m_cycle{};
+  unsigned m_cols;
+  unsigned m_rows;
+  hai::array<char> m_data;
+
+public:
+  map_t(unsigned rows, unsigned cols)
+      : m_cols{cols}, m_rows{rows}, m_data{rows * cols} {
+    if (rows != cols)
+      throw 1;
+  }
+
+  auto begin() { return m_data.begin(); }
+
+  auto copy() {
+    hai::cstr res{m_cols * m_rows};
+    auto p = m_data.begin();
+    for (auto &c : res) {
+      c = *p++;
+    }
+    return res;
+  }
+
+  char &get(int r, int c) {
+    switch (m_cycle) {
+    case 0:
+      return m_data[r * m_cols + c];
+    case 1:
+      return m_data[c * m_cols + r];
+    case 2:
+      r = m_rows - r - 1;
+      return m_data[r * m_cols + c];
+    case 3:
+      r = m_rows - r - 1;
+      return m_data[c * m_cols + r];
+    default:
+      throw 0;
+    }
+  }
+
+  void cycle(unsigned c) {
+    m_cycle = c;
+    for (auto c = 0; c < m_cols; c++) {
+      int rr = 0;
+      for (auto r = 0; r < m_rows; r++) {
+        switch (get(r, c)) {
+        case '.':
+          break;
+        case 'O':
+          get(r, c) = '.';
+          get(rr, c) = 'O';
+          rr++;
+          break;
+        case '#':
+          rr = r + 1;
+          break;
+        }
+      }
+    }
+  }
+
+  void full_cycle() {
+    for (auto c = 0; c < 4; c++)
+      cycle(c);
+  }
+};
+
+bool operator==(const hai::cstr &a, const char *b) {
+  for (auto c : a) {
+    if (c != *b++)
+      return false;
+  }
+  return true;
+}
+
 int main(int argc, char **argv) {
   auto dt = data::of(argc);
 
@@ -13,55 +89,55 @@ int main(int argc, char **argv) {
     cols = row.size();
     rows++;
   }
-  hai::array<char> map{rows * cols};
+  map_t map{rows, cols};
   auto p = map.begin();
   for (auto row : dt) {
     for (auto c : row)
       *p++ = c;
   }
 
-  for (auto i = 0; i < rows; i++)
-    silog::log(silog::debug, "%.*s", cols, map.begin() + i * cols);
+  struct bleh_t {
+    hai::cstr map;
+    long res;
+    long i;
+  };
+  hai::varray<bleh_t> bleh{102400};
+  constexpr const long limit = 1000000000;
+  for (long i = 0; i < limit; i++) {
+    map.full_cycle();
 
-  for (auto c = 0; c < cols; c++) {
-    int rr = 0;
-    for (auto r = 0; r < rows; r++) {
-      switch (map[c + r * cols]) {
-      case '.':
-        break;
-      case 'O':
-        map[c + r * cols] = '.';
-        map[c + rr * cols] = 'O';
-        rr++;
-        break;
-      case '#':
-        rr = r + 1;
-        break;
-      }
+    hai::array<long> pts{cols};
+    for (auto &p : pts) {
+      p = rows;
     }
-  }
 
-  silog::log(silog::debug, "");
-  for (auto i = 0; i < rows; i++)
-    silog::log(silog::debug, "%.*s", cols, map.begin() + i * cols);
-
-  hai::array<long> pts{cols};
-  for (auto &p : pts) {
-    p = rows;
-  }
-
-  int r{};
-  long res{};
-  for (auto i = 0; i < rows; i++) {
-    auto row = map.begin() + r * cols;
-    for (int c{}; c < cols; c++) {
-      if (row[c] == 'O') {
-        res += pts[c]--;
-      } else if (row[c] == '#') {
-        pts[c] = rows - r - 1;
+    int r{};
+    long res{};
+    for (auto i = 0; i < rows; i++) {
+      auto row = map.begin() + r * cols;
+      for (int c{}; c < cols; c++) {
+        if (row[c] == 'O') {
+          res += rows - r;
+        }
       }
+      r++;
     }
-    r++;
+
+    // for (auto i = 0; i < rows; i++)
+    //   silog::log(silog::debug, "%.*s", cols, map.begin() + i * cols);
+
+    silog::log(silog::debug, "%10ld %ld", i, res);
+    for (const auto &b : bleh) {
+      if (res != b.res)
+        continue;
+      if (b.map != map.begin())
+        continue;
+
+      info("i", i);
+      info("b.i", b.i);
+      long x = (limit - b.i) % (i - b.i);
+      i = limit - x;
+    }
+    bleh.push_back(bleh_t{map.copy(), res, i});
   }
-  info("res", res);
 }
