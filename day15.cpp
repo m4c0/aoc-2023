a6f65f15f6e4c1dce158afd9a470f792372ac6bf
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-15 16:21:39 +0000

    day 15 - part 2 - too easy... no fun

--- day15.cpp
+++ day15.cpp
@@ -15,15 +15,93 @@ constexpr auto hash(jute::view v) {
 }
 static_assert(hash("HASH") == 52);
 
+struct bucket {
+  jute::view key;
+  int v;
+  bucket *next;
+  bucket *prev;
+};
+struct blist {
+  bucket *head;
+  bucket *tail;
+};
+
+constexpr auto split(jute::view v) {
+  struct {
+    jute::view a;
+    char op;
+    int num{};
+  } res;
+
+  auto [lbl, r] = v.split('=');
+  if (r != "") {
+    res.a = lbl;
+    res.op = '=';
+    res.num = atoi(r);
+  } else {
+    auto [lbl, r] = v.split('-');
+    res.a = lbl;
+    res.op = '-';
+  }
+  return res;
+}
+
 int main(int argc, char **argv) {
   auto dt = data::of(argc);
   auto line = *(dt.begin());
 
-  int sum{};
+  blist map[256]{};
+
   while (line != "") {
     auto [l, r] = line.split(',');
-    sum += hash(l);
+    auto [lbl, op, v] = split(l);
+
+    auto h = hash(lbl);
+    auto &bl = map[h];
+    auto p = bl.head;
+    while (p) {
+      if (p->key == lbl) {
+        break;
+      }
+      p = p->next;
+    }
+
+    if (op == '-') {
+      if (p) {
+        if (p == bl.head)
+          bl.head = bl.head->next;
+        if (p == bl.tail)
+          bl.tail = bl.tail->prev;
+        if (p->prev)
+          p->prev->next = p->next;
+        if (p->next)
+          p->next->prev = p->prev;
+      }
+    } else if (p) {
+      p->v = v;
+    } else {
+      auto n = new bucket{lbl, v, nullptr, bl.tail};
+      if (bl.tail) {
+        bl.tail->next = n;
+      } else {
+        bl.head = n;
+      }
+      bl.tail = n;
+    }
+
     line = r;
+  }
+
+  int sum{};
+  for (auto b = 0; b < 256; b++) {
+    auto i = 1;
+    auto p = map[b].head;
+    while (p) {
+      silog::log(silog::debug, "%d %.2s %d", b, p->key.data(), p->v);
+      sum += (b + 1) * i * p->v;
+      i++;
+      p = p->next;
+    }
   }
   info("sum", sum);
 }
