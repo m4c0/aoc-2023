e1631b2a3d74b0c84a8dfe97066837cc62173c0e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-20 11:27:31 +0000

    day 19 - part 2
    at least I know how to do intervals. Just lack self-confidence or
    something.

--- day19.cpp
+++ day19.cpp
@@ -6,14 +6,10 @@ import scanf;
 import silog;
 
 struct part {
-  long n[4]; // x m a s
+  // x m a s
+  long min[4]{1, 1, 1, 1};
+  long max[4]{4000, 4000, 4000, 4000};
 };
-constexpr part top(jute::view line) {
-  part res{};
-  scan::f(line, "{x=\v,m=\v,a=\v,s=\v}", res.n[0], res.n[1], res.n[2],
-          res.n[3]);
-  return res;
-}
 
 constexpr const auto A = 1000000;
 constexpr const auto R = 2000000;
@@ -37,13 +33,43 @@ static_assert(tok("R") == R);
 static_assert(tok("aaa") == 813);
 static_assert(tok("aab") == 814);
 
-constexpr bool lt(long a, long b) { return a < b; }
-constexpr bool gt(long a, long b) { return a > b; }
+struct qpart : part {
+  int wf{tok("in")};
+};
+void info(const char *lbl, int col, const qpart &p) {
+  silog::log(silog::info,
+             "\e[0;%dm%8s: x=[%4ld;%4ld] m=[%4ld;%4ld] a=[%4ld;%4ld] "
+             "s=[%4ld;%4ld]\e[0m wf:%d",
+             col, lbl, p.min[0], p.max[0], p.min[1], p.max[1], p.min[2],
+             p.max[2], p.min[3], p.max[3], p.wf);
+}
+
+using fn_t = void (*)(long &, long &, long);
+constexpr void lt(long &min, long &max, long thr) {
+  mn(min, thr - 1);
+  mn(max, thr - 1);
+}
+static_assert([] {
+  long min = 1;
+  long max = 4000;
+  lt(min, max, 1665);
+  return min == 1 && max == 1664;
+}());
+
+constexpr void gt(long &min, long &max, long thr) {
+  mx(min, thr + 1);
+  mx(max, thr + 1);
+}
+static_assert([] {
+  long min = 1;
+  long max = 4000;
+  gt(min, max, 1665);
+  return min == 1666 && max == 4000;
+}());
 
 namespace scan {
-template <> struct parse<bool (*)(long, long)> {
-  constexpr bool operator()(jute::view &in, jute::view &fmt,
-                            bool (*&v)(long, long)) {
+template <> struct parse<fn_t> {
+  constexpr bool operator()(jute::view &in, jute::view &fmt, fn_t &v) {
     if (in[0] == '<') {
       v = lt;
     } else if (in[0] == '>') {
@@ -59,7 +85,7 @@ template <> struct parse<bool (*)(long, long)> {
 
 struct inst {
   int lhs{-1};
-  bool (*op)(long, long){};
+  fn_t op{};
   long rhs{};
   int outcome{};
 };
@@ -68,6 +94,7 @@ struct wflow {
   inst insts[10]{};
   int icount{};
   int fallback{};
+  jute::view line{};
 };
 constexpr int toi(char c) {
   switch (c) {
@@ -89,7 +116,7 @@ constexpr wflow tow(jute::view line) {
   if (!scan::f(line, "\v{\v}", key, ops))
     throw 0;
 
-  wflow res{.key = tok(key)};
+  wflow res{.key = tok(key), .line = line};
 
   for (auto op : split_it<','>{ops}) {
     inst &i = res.insts[res.icount];
@@ -122,57 +149,95 @@ static_assert(test.insts[1].op == gt);
 static_assert(test.insts[1].rhs == 2090);
 static_assert(test.insts[1].outcome == A);
 
-hai::array<wflow> wfs{30 * 30 * 30};
-
-long run(jute::view line) {
-  auto xmas = top(line);
-  const auto *inst = &wfs[tok("in")];
-
-  while (true) {
-    if (inst->icount == 0)
-      throw 0;
-
-    int outcome = inst->fallback;
-    for (auto i = 0; i < inst->icount; i++) {
-      auto &op = inst->insts[i];
-      auto lhs = xmas.n[op.lhs];
-      if (!op.op(lhs, op.rhs)) {
-        continue;
-      }
-      outcome = op.outcome;
-      break;
-    }
-
-    if (outcome == R) {
-      info("reject", 0);
-      return 0;
-    }
-    if (outcome == A) {
-      info("accept", xmas.n[0] + xmas.n[1] + xmas.n[2] + xmas.n[3]);
-      return xmas.n[0] + xmas.n[1] + xmas.n[2] + xmas.n[3];
-    }
-
-    info("loop", outcome);
-    inst = &wfs[outcome];
+qpart apply(const inst &op, qpart &q) {
+  auto lhs = op.lhs;
+  if (q.min[lhs] > q.max[lhs]) {
+    return q;
   }
+
+  qpart qc = q;
+  op.op(qc.min[lhs], qc.max[lhs], op.rhs);
+  if (qc.min[lhs] > q.min[lhs]) {
+    q.max[lhs] = qc.min[lhs] - 1;
+  }
+  if (qc.max[lhs] < q.max[lhs]) {
+    q.min[lhs] = qc.max[lhs] + 1;
+  }
+  return qc;
+}
+bool valid(const qpart &q) {
+  for (auto i = 0; i < 4; i++) {
+    if (q.min[i] > q.max[i])
+      return false;
+  }
+  return true;
 }
 
 int main(int argc, char **argv) {
   auto dt = data::of(argc);
+  hai::array<wflow> wfs{30 * 30 * 30};
 
-  long res{};
-  bool running{};
   for (auto line : dt) {
-    info("line", line);
     if (line == "") {
-      running = true;
-    } else if (!running) {
-      auto i = tow(line);
-      wfs[i.key] = i;
-    } else {
-      res += run(line);
+      break;
     }
+    auto i = tow(line);
+    wfs[i.key] = i;
   }
 
+  hai::varray<qpart> queue{102400};
+  queue.push_back(qpart{});
+
+  int rcount{};
+  int acount{};
+  long res{};
+  const auto oc = [&](auto outcome, auto &q) {
+    if (outcome == R) {
+      rcount++;
+      info("reject", 31, q);
+    } else if (outcome == A) {
+      acount++;
+      info("accept", 33, q);
+
+      if (valid(q)) {
+        long rate{1};
+        for (auto i = 0; i < 4; i++) {
+          rate *= q.max[i] - q.min[i] + 1;
+        }
+        res += rate;
+      }
+    } else {
+      q.wf = outcome;
+      info("enqueue", 35, q);
+      queue.push_back(q);
+    }
+  };
+
+  while (queue.size() > 0) {
+    auto q = queue.pop_back();
+    const auto *inst = &wfs[q.wf];
+    silog::log(silog::info, "-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    info("dequeue", 37, q);
+    info("   wflow", inst->line);
+
+    for (auto i = 0; i < inst->icount; i++) {
+      info("check", 32, q);
+      auto &op = inst->insts[i];
+
+      auto lhs = op.lhs;
+      qpart qc = apply(op, q);
+
+      if (qc.min[lhs] <= qc.max[lhs]) {
+        oc(op.outcome, qc);
+      }
+    }
+
+    oc(inst->fallback, q);
+  }
+
+  info("r", rcount);
+  info("a", acount);
+
+  info("exp", 167409079868000L);
   info("res", res);
 }
