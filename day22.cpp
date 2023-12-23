00913d21706e17b5005616bec4176370b893effa
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-23 15:31:42 +0000

    day 22 - part 1 - redone with graphs

--- day22.cpp
+++ day22.cpp
@@ -1,4 +1,5 @@
 #pragma leco tool
+#include <stdio.h>
 import bruh;
 import hai;
 import jute;
@@ -40,9 +41,12 @@ template <typename Tp> class list {
 
   node *m_head{};
   node *m_tail{};
+  int m_size{};
 
 public:
   constexpr void push_back(Tp v) {
+    m_size++;
+
     auto n = new node{.value = v};
     if (m_head == nullptr) {
       m_head = m_tail = n;
@@ -55,6 +59,18 @@ public:
 
   constexpr auto begin() noexcept { return it{m_head}; }
   constexpr auto end() noexcept { return it{}; }
+  constexpr auto begin() const noexcept { return it{m_head}; }
+  constexpr auto end() const noexcept { return it{}; }
+
+  constexpr int size() const noexcept { return m_size; }
+  constexpr bool empty() const noexcept { return m_head == nullptr; }
+  constexpr bool contains(const Tp &v) const noexcept {
+    for (auto &n : *this) {
+      if (n == v)
+        return true;
+    }
+    return false;
+  }
 };
 } // namespace ll
 
@@ -95,6 +111,26 @@ void dump(const auto &blocks) {
     silog::log(silog::info, "%d %d %d - %d %d %d", b.x0, b.y0, b.z0, b.x1, b.y1,
                b.z1);
   }
+}
+const char *str(int n) {
+  static char buf[10]{};
+  char *res = buf;
+  while (n > 0) {
+    *res++ = (n % 26) + 'A' - 1;
+    n /= 26;
+  }
+  *res = 0;
+  return buf;
+}
+void dump_g(const auto &graph) {
+  for (auto i = 0; i < graph.size(); i++) {
+    fprintf(stderr, "%4d %4s: ", i, str(i));
+    for (auto j : graph[i]) {
+      fprintf(stderr, "%4s ", str(j));
+    }
+    fprintf(stderr, "\n");
+  }
+  fprintf(stderr, "\n");
 }
 
 int main(int argc, char **argv) {
@@ -154,31 +190,32 @@ int main(int argc, char **argv) {
 
   // dump(blocks);
 
-  int safe_count{};
-  for (auto blasted = blocks.begin(); blasted != blocks.end(); ++blasted) {
-    bool safe{true};
-    for (auto a = blocks.begin(); a != blocks.end(); ++a) {
-      if (a == blasted)
-        continue;
-      if (a->z0 == 1)
-        continue;
+  hai::array<ll::list<int>> sups{blocks.size() + 1};
+  hai::array<ll::list<int>> is_sup_by{blocks.size() + 1};
+  for (auto a = blocks.begin(); a != blocks.end(); ++a) {
+    if (a->z0 == 1)
+      continue;
 
-      auto a1 = fall1(*a);
-      bool hit{};
-      for (auto b = blocks.begin(); b != blocks.end(); ++b) {
-        if (blasted != b && a != b && hits(a1, *b)) {
-          hit = true;
-          break;
-        }
+    auto a1 = fall1(*a);
+    for (auto b = blocks.begin(); b != blocks.end(); ++b) {
+      if (a != b && hits(a1, *b)) {
+        sups[b->n].push_back(a->n);
+        is_sup_by[a->n].push_back(b->n);
       }
-      if (!hit) {
-        safe = false;
+    }
+  }
+
+  dump_g(sups);
+  dump_g(is_sup_by);
+
+  long part1{blocks.size()};
+  for (auto &b : blocks) {
+    for (auto s : sups[b.n]) {
+      if (is_sup_by[s].size() == 1) {
+        part1--;
         break;
       }
     }
-    if (safe)
-      safe_count++;
   }
-
-  info("done", safe_count);
+  info("part 1", part1);
 }
