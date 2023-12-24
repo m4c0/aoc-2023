cb03d18b8cee5bb7719ddee0f2f99760b2e71c7d
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-24 19:11:30 +0000

    day 23 - part 2 - no idea what i'm doing

--- day23.cpp
+++ day23.cpp
@@ -14,6 +14,7 @@ struct node {
 data_map map;
 bool vis[150][150]{};
 node paths[150][150][4]{};
+bool gvis[150][150]{};
 
 // Using 1-based + VIM-like coords
 void dump(const char *msg, point a, point b, long sz) {
@@ -25,10 +26,19 @@ void dump_map() {
   for (point p{}; p.y < map.rows; p.y++) {
     for (p.x = 0; p.x < map.cols; p.x++) {
       auto pp = p + point{1, 1};
-      if (vis[pp.y][pp.x]) {
-        fprintf(stderr, "O");
-      } else
-        fprintf(stderr, "%c", map.at(p));
+      if (paths[pp.x][pp.y][0].sz > 0) {
+        fprintf(stderr, "\e[7m");
+      }
+
+      if (map.at(p) == '#') {
+        fprintf(stderr, "#");
+      } else if (vis[pp.y][pp.x]) {
+        fprintf(stderr, ".");
+      } else {
+        fprintf(stderr, " ");
+      }
+
+      fprintf(stderr, "\e[0m");
     }
     fprintf(stderr, "\n");
   }
@@ -48,7 +58,7 @@ void link(point a, point b, long sz) {
     if (n.sz == 0) {
       n.p = b;
       n.sz = sz;
-      // dump("link", a, b, sz);
+      dump("link", a, b, sz);
       return;
     }
   }
@@ -75,6 +85,7 @@ void trace(point from, point p, long sz) {
   }
   if (paths > 2) {
     link(from, p, sz);
+    link(p, from, sz);
     from = p;
     sz = 0;
   }
@@ -85,14 +96,27 @@ void trace(point from, point p, long sz) {
   // vis[p.y][p.x] = false;
 }
 
-long walk(point p) {
+long walk(point p, long cost = 0) {
+  if (p.y == map.rows)
+    return cost;
+
+  auto &gv = gvis[p.x][p.y];
+  if (gv)
+    return 0;
+  gv = true;
+
   long res{};
   for (auto &n : paths[p.x][p.y]) {
     if (n.sz == 0)
       continue;
 
-    mx(res, n.sz + walk(n.p));
+    long wtf = walk(n.p, n.sz);
+    mx(res, cost + wtf);
+    // dump("walk", p, n.p, n.sz);
+    // silog::log(silog::info, "wtf: %ld %ld", wtf, res);
   }
+
+  gv = false;
   return res;
 }
 
@@ -103,7 +127,7 @@ int main(int argc, char **argv) {
   const point s{2, 1};
   trace(s, s, 0);
 
-  // dump_map();
-
   info("res", walk(s));
+
+  // dump_map();
 }
